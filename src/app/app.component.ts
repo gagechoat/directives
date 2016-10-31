dd1f7f8e82033b126aa1bd3e7b3d059fe1cf1c55
gagechoat <gagechoat@gmail.com>
2016-10-30 22:22:27 -0700

    complete lesson 46

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dir works!';
+  private switch = true;
+
+  onSwitch() {
+    this.switch = !this.switch;
+  }
 }
--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,15 +1,26 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, HostListener, HostBinding, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective {
+  @HostListener('mouseenter') mouseover() {
+    this.backgroundColor = this.highlightColor;
+  };
+  @HostListener('mouseleave') mouseleave() {
+    this.backgroundColor = this.defaultColor;
+  };
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  };
+  @Input() defaultColor = 'white';
+  @Input('highlight') highlightColor = 'red';
+  private backgroundColor = this.defaultColor;
 
-  constructor(
-    private elementRef: ElementRef,
-    private renderer: Renderer
-  ) {
-    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', '#aff');
+  constructor() {}
+
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
   }
 
 }
