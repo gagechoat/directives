733235cb43278eb8ce31978cf9c2671bd91126e0
gagechoat <gagechoat@gmail.com>
2016-10-30 22:01:59 -0700

    finish lesson 42

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef, Renderer } from '@angular/core';
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective {
+
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer
+  ) {
+    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', '#aff');
+  }
+
+}
